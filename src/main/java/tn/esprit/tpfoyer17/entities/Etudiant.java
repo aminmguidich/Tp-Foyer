38554e429ae47a359dc3683724b199ccda4487ea
YoussefGhrir <gheriryoussef@gmail.com>
2024-11-07 14:28:35 +0100

    test unitaire

--- src/main/java/tn/esprit/tpfoyer17/entities/Etudiant.java
+++ src/main/java/tn/esprit/tpfoyer17/entities/Etudiant.java
@@ -21,7 +21,6 @@ public class Etudiant implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
     long idEtudiant;
 
     String nomEtudiant;

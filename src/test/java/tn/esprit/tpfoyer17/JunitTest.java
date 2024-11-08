f9230baafc89853f56f59854b5677bc7fbe33121
hama <ayari.mohamedamine@esprit.tn>
2024-11-08 00:27:14 +0000

    coverage

--- src/test/java/tn/esprit/tpfoyer17/JunitTest.java
+++ src/test/java/tn/esprit/tpfoyer17/JunitTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNull;
 @SpringBootTest
-public class JunitTest {
+ class JunitTest {
     @Autowired
     private UniversiteService universiteService;
 

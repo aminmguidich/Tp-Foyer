c860621e03093bcb65bcd65c4e162fc533d83a75
YoussefGhrir <gheriryoussef@gmail.com>
2024-11-14 23:22:26 +0100

    qlq crx

--- src/main/java/tn/esprit/tpfoyer17/repositories/UniversiteRepository.java
+++ src/main/java/tn/esprit/tpfoyer17/repositories/UniversiteRepository.java
@@ -11,9 +11,6 @@ import java.util.List;
 @Repository
 public interface UniversiteRepository extends CrudRepository<Universite,Long> {
     Universite findByNomUniversiteLike(String nomu);
-
-   // List<Chambre> findByNomUniversiteLikeAndFoyerBlocsChambres(String nom);
-
     List<Reservation>
     findByFoyerBlocsChambresReservationsAnneeUniversitaireAndNomUniversite(
             LocalDate anneeUniversite,
